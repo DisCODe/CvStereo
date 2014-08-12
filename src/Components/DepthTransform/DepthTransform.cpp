255112c1b163cd4b94e92144d85376cb2aa00a51
Tomasz Kornuta <tkornuta@gmail.com>
2014-08-12 16:34:25 +0200

    pull depthtransform from lzmuda - not testedgit status

--- src/Components/DepthTransform/DepthTransform.cpp
+++ src/Components/DepthTransform/DepthTransform.cpp
@@ -35,8 +35,7 @@ DepthTransform::~DepthTransform() {
 void DepthTransform::prepareInterface() {
 
 	// Register data streams, events and event handlers HERE!
-	registerStream("in_tvec", &in_tvec);
-	registerStream("in_rvec", &in_rvec);
+	registerStream("in_homogMatrix", &in_homogMatrix);
 	registerStream("in_image_xyz", &in_image_xyz);
 	registerStream("out_image_xyz", &out_image_xyz);
 
@@ -67,65 +66,72 @@ bool DepthTransform::onStart() {
 void DepthTransform::DepthTransformation() {
 	try{
 	  
-	  HomogMatrix hm;
+	  
+	
+	  
 	  cv::Mat depth_image = in_image_xyz.read();
-	  cv::Mat tvec = in_tvec.read();
-	  cv::Mat rvec = in_rvec.read();
-	  cv::Mat transformed_image;
+	  HomogMatrix hm = in_homogMatrix.read();
+	  
+	  stringstream ss;
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			ss << hm.elements[i][j] << "  ";
+		}
+	}
+	CLOG(LINFO) << "HomogMatrix:\n" << ss.str() << endl;
+	  
+	  
+	  
 	  cv::Size depth_size = depth_image.size();
-	  std::cout<<"tvec : "<<tvec<<"\n";
-	  std::cout<<"rvec : "<<rvec<<"\n";
+
+	  cv::Mat_<double> rotationMatrix(3,3);
+	  cv::Mat_<double> tvec(3,1);	  
+	  
 	  cv::Mat tmp_img;// = cv::Mat(depth_image);
 	  tmp_img.create(depth_size, CV_32FC3);
-	  //////////////////////////////////////////////////////////////////////////
-	  
-	// iteracja po obrazie glebi, podstawienie pod maske odpowiednich pikseli
-        if (depth_image.isContinuous() && tmp_img.isContinuous()) {
-         depth_size.width *= depth_size.height;
-         depth_size.height = 1;
-        }
-        
+	    
             float newX, newY, newZ;
             depth_size.width *= 3;
-	
 	    
-	    ////////////////////////////////////////
-	    /*
-	    HomogMatrix hm;
+	    
+	    rotationMatrix.at<double>(0,0)=hm.elements[0][0];
+	    rotationMatrix.at<double>(0,1)=hm.elements[0][1];
+	    rotationMatrix.at<double>(0,2)=hm.elements[0][2];
+	    
+	    rotationMatrix.at<double>(1,0) = hm.elements[1][0]; 
+	    rotationMatrix.at<double>(1,1) = hm.elements[1][1];
+	    rotationMatrix.at<double>(1,2) = hm.elements[1][2];
+	    
+	    rotationMatrix.at<double>(2,0)=hm.elements[2][0];
+	    rotationMatrix.at<double>(2,1)=hm.elements[2][1];
+	    rotationMatrix.at<double>(2,2)=hm.elements[2][2];
+	   
+	    tvec.at<double>(0,0) = hm.elements[0][3];
+	    tvec.at<double>(1,0) = hm.elements[1][3];
+	    tvec.at<double>(2,0) = hm.elements[2][3];
+	    
+	    LOG(LINFO)<< " rotationMatrix " << rotationMatrix;
+	    
+	    LOG(LINFO)<<"hm" <<hm.elements[3][0] << hm.elements[3][1]<<hm.elements[3][2];
 
-	    hm.elements[0][0] = rvec.at<double>(0,0);
-	    hm.elements[0][1] = rvec.at<double>(0,1);
-	    hm.elements[0][2] = rvec.at<double>(0,2);
-	    hm.elements[0][3] = 0;
+	   
+	    LOG(LINFO)<< " tvec " << tvec;
+	   
+	    rotationMatrix=rotationMatrix.t();
 	    
-	    hm.elements[1][0] = rvec.at<double>(1,0);
-	    hm.elements[1][1] = rvec.at<double>(1,1);
-	    hm.elements[1][2] = rvec.at<double>(1,2);
-	    hm.elements[1][3] = 0;
-	    
-	    hm.elements[2][0] = rvec.at<double>(2,0);
-	    hm.elements[2][1] = rvec.at<double>(2,1);
-	    hm.elements[2][2] = rvec.at<double>(2,2);
-	    hm.elements[2][3] = 0;
 
-	    hm.elements[3][0] = 0;
-	    hm.elements[3][1] = 0;
-	    hm.elements[3][2] = 0;
-	    hm.elements[3][3] = 1;
-	    
-	    */
-	
-	    ///////////////////////////////////////
-	    
+	//LOG(LINFO) << "DepthTransformation\n";
         for (int i = 0; i < depth_size.height; i++)
         {
+	  
             const float* depth_ptr = depth_image.ptr <float> (i);
 	    float* depth_ptr_tmp = tmp_img.ptr <float> (i);
             int j, k = 0;
 	    int val = 0;
-	    
+	   
             for (j = 0; j < depth_size.width; j += 3)
             {
+	      
 		 // get x, y, z from depth_image
 		 float x = depth_ptr[j];
                  float y = depth_ptr[j + 1];
@@ -138,45 +144,22 @@ void DepthTransform::DepthTransformation() {
 		      depth_ptr_tmp[j + 2]=z;
 		      continue;
 		  }
-
-		 
-		// float x=1,y=5,z=10;
-		//std::cout<<"Bylo: "<<x<<" "<<y<<" "<<z<<"\n";
 		
 		x=x-tvec.at<double>(0,0);
 		y=y-tvec.at<double>(1,0);
 		z=z-tvec.at<double>(2,0);
-		//std::cout<<"Jest: "<<x<<" "<<y<<" "<<z<<"\n";
 
-		rvec=rvec.t();
-      		newX = x*rvec.at<double>(0,0) + y*rvec.at<double>(0,1)+z*rvec.at<double>(0,2);
-		newY = x*rvec.at<double>(1,0) + y*rvec.at<double>(1,1)+z*rvec.at<double>(1,2);
-		newZ = x*rvec.at<double>(2,0) + y*rvec.at<double>(2,1)+z*rvec.at<double>(2,2);
-		//std::cout<<"Jest po rotacji: "<<newX<<" "<<newY<<" "<<newZ<<"\n";
+      		newX = x*rotationMatrix.at<double>(0,0) + y*rotationMatrix.at<double>(0,1)+z*rotationMatrix.at<double>(0,2);
+		newY = x*rotationMatrix.at<double>(1,0) + y*rotationMatrix.at<double>(1,1)+z*rotationMatrix.at<double>(1,2);
+		newZ = x*rotationMatrix.at<double>(2,0) + y*rotationMatrix.at<double>(2,1)+z*rotationMatrix.at<double>(2,2);
 
 		depth_ptr_tmp[j]=newX;
                 depth_ptr_tmp[j + 1]=newY;
                 depth_ptr_tmp[j + 2]=newZ;
+		
             }
         }
-        /*
-        for (int i = 0; i < depth_size.height; i++)
-        {
-	    float* depth_ptr_tmp = tmp_img.ptr <float> (i);
-            int j, k = 0;
-	    int val = 0;
-	    
-            for (j = 0; j < depth_size.width; j += 3)
-            {
-		std::cout << "Czytam: "<<depth_ptr_tmp[j]<<" "<<depth_ptr_tmp[j + 1]<<" "<<depth_ptr_tmp[j + 2]<<"\n";
-            }
-        }
-        */
-        
-        
-        
-        
-        
+       
 	  LOG(LINFO) << "Writing to data stream";
 	  out_image_xyz.write(tmp_img);
 	  
